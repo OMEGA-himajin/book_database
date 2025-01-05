e67fb81340ba07146551ba9ffa59118b0841027e
OMEGA <lightsound2321@gmail.com>
2025-01-05 13:08:47 +0900

    add: added project files

---
+++ src/LibMan/src/main/java/com/libman/demo/LibManApplication.java
@@ -0,0 +1,13 @@
+package com.libman.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibManApplication.class, args);
+	}
+
+}
---
+++ src/LibMan/src/test/java/com/libman/demo/LibManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
